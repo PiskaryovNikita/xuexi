2fd34f227340f7ddfe3216a8e8d22735feed3b7f
Mykyta_Piskarov <Mykyta_Piskarov@epam.com>
2020-11-19 12:44:41 +0200

    Add example of prototype and proxy to demonstrate that each proxy method invocation will get new object

--- spring/src/main/java/com/xuexi/spring/scope/ColorFrame.java
+++ spring/src/main/java/com/xuexi/spring/scope/ColorFrame.java
@@ -2,10 +2,16 @@ package com.xuexi.spring.scope;
 
 import java.awt.Color;
 import java.util.Random;
+
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-public abstract class ColorFrame extends JFrame {
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ColorFrame extends JFrame {
+    private static final long serialVersionUID = -8417257481647671943L;
+    @Autowired
+    private Color color;
 
     public ColorFrame() {
         setSize(200, 200);
@@ -16,9 +22,7 @@ public abstract class ColorFrame extends JFrame {
     public void showOnRandomPlace() {
         Random random = new Random();
         setLocation(random.nextInt(1200), random.nextInt(700));
-        getContentPane().setBackground(getColor());
+        getContentPane().setBackground(color);
         repaint();
     }
-
-    protected abstract Color getColor();
 }
--- spring/src/main/java/com/xuexi/spring/scope/Main.java
+++ spring/src/main/java/com/xuexi/spring/scope/Main.java
@@ -2,42 +2,47 @@ package com.xuexi.spring.scope;
 
 import java.awt.Color;
 import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 @Configuration
 public class Main {
     @Autowired
     private ApplicationContext context;
 
+    /**
+     * Combination of "prototype and proxy" will result new target bean for each proxy method invocation.
+     * 1. Execute proxy method
+     * 2. Cglib intercept, targetSource.getTarget() does beanFactory.getBean that will return new object
+     * 3. Method invoked on new object
+     * <p>
+     * The same logic will work with proxy that is created using Java Dynamic Proxy
+     */
     @Bean
-    @Scope(scopeName = "prototype")
+    @Scope(scopeName = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
     public Color color() {
         Random random = new Random();
         return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
     }
 
     @Bean
-    @Scope(scopeName = "singleton")
     public ColorFrame colorFrame() {
-        return new ColorFrame() {
-            @Override
-            protected Color getColor() {
-                return context.getBean(Color.class);
-            }
-        };
+        return new ColorFrame();
     }
 
     public static void main(String[] args) throws InterruptedException {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(Main.class);
         while (true) {
-            Thread.sleep(100);
-            context.getBean(ColorFrame.class).showOnRandomPlace();
-
+            ColorFrame colorFrame = context.getBean(ColorFrame.class);
+            colorFrame.showOnRandomPlace();
+            Thread.sleep(200);
         }
     }
 }
